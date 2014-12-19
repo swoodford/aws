da3bd8e4f36c8cf374e9f779fc1e416c44ff82c7
Shawn Woodford <shawn@kingandpartners.com>
2014-12-19 10:21:36 -0500

    Import CIDR IP list to AWS VPC ACL rule and deny access

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ A collection of shell scripts for automating various tasks with Amazon Web Servi
 - **create-cloudwatch-alarms.sh** Create AWS CloudWatch alarms for EC2, RDS, Load Balancer environments
 - **ec2-create-snapshots.sh** Create a snapshot of each EC2 volume that is tagged with the backup flag
 - **ec2-delete-snapshots.sh** Deletes snapshots for each EC2 volume that is tagged with the backup flag and matches the specified date
+- **ec2-import-network-acl.sh** Import CIDR IP list to AWS VPC ACL rule and deny access
 - **install-awscli.sh** Install and configure AWS CLI
 - **install-s3cmd.sh** Install and setup s3cmd from the GitHub Repo
 - **s3-buckets-file-size.sh** Count total size of all data stored in all S3 buckets
---
+++ ec2-import-network-acl.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# This script will read from the list of CIDR IPs in the file ipblacklistmaster 
+# Then create an AWS VPC ACL rule to deny access to each CIDR IP specified
+
+# VERY IMPORTANT to set the correct Network ACL ID for the intended ACL
+NETWORKACLID="YOUR-ACL-ID-HERE"
+
+# Verify AWS CLI Credentials are setup
+# http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html
+if ! grep -q aws_access_key_id ~/.aws/config; then
+	if ! grep -q aws_access_key_id ~/.aws/credentials; then
+		echo "Error: AWS config not found or CLI not installed. Please run \"aws configure\"."
+		exit 1
+	fi
+fi
+
+# exec &>> ~/vpcacl.log
+echo "============================="
+date '+%c'
+echo "============================="
+
+TOTALCIDR=$(wc -l ipblacklistmaster | cut -d " " -f5)
+
+echo " "
+echo "====================================================="
+echo "Adding CIDR records to ACL"
+echo "Records to be created: "$TOTALCIDR
+echo "====================================================="
+echo " "
+
+START=1
+for (( COUNT=$START; COUNT<=$TOTALCIDR; COUNT++ ))
+do
+	echo "====================================================="
+	echo "Rule "\#$COUNT
+
+	CIDR=$(nl ipblacklistmaster | grep -w [^0-9][[:space:]]$COUNT | cut -f 2)
+	echo "CIDR="$CIDR
+
+	# echo "Network ACL ID: "$NETWORKACLID
+
+	ADDRECORD=$(aws ec2 create-network-acl-entry --network-acl-id $NETWORKACLID --rule-number $COUNT --protocol -1 --rule-action deny --ingress --cidr-block $CIDR)
+	echo "Record created: "$ADDRECORD
+done
+
+echo "====================================================="
+echo " "
+echo "Completed!"
+echo " "
