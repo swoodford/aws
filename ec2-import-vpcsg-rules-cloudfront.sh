47b96acd643ab8df5fdc4281c05c260fdd90f7f3
Shawn Woodford <sw@shawnwoodford.com>
2015-09-09 11:47:29 -0400

    Create VPC Security Group with Cloudfront IP ranges

---
+++ ec2-import-vpcsg-rules-cloudfront.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# Create VPC Security Group with Cloudfront IP ranges
+
+# Get current list of Cloudfront IP ranges
+IPLIST=$(curl -s https://ip-ranges.amazonaws.com/ip-ranges.json | jq '.prefixes | .[] | select(.service=="CLOUDFRONT") | .ip_prefix' | cut -d '"' -f2)
+
+# Set Variables
+GROUPNAME="Cloudfront"
+DESCR="Cloudfront IP Ranges"
+VPCID=$(aws ec2 describe-vpcs --output=json | jq '.Vpcs | .[] | .VpcId' | cut -d '"' -f2)
+PROTO="tcp"
+PORT="80"
+TOTALIPS=$(echo "$IPLIST" | wc -l)
+
+# Functions
+function pause(){
+	read -p "Press any key to continue..."
+	echo
+}
+
+function check_command {
+	type -P $1 &>/dev/null || fail "Unable to find $1, please install it and run this script again."
+}
+
+function fail(){
+	tput setaf 1; echo "Failure: $*" && tput sgr0
+	exit 1
+}
+
+function addRules (){
+	# Check for existing security group or create new one
+	if ! aws ec2 describe-security-groups --output=json | jq '.SecurityGroups | .[] | .GroupName' | grep -q Cloudfront; then
+		echo
+		echo "====================================================="
+		echo "Creating Security Group "$GROUPNAME
+		GROUPID=$(aws ec2 create-security-group --group-name "$GROUPNAME" --description "$DESCR" --vpc-id $VPCID)
+		aws ec2 create-tags --resources $(aws ec2 describe-security-groups --output=json | jq '.SecurityGroups | .[] | select(.GroupName=="Cloudfront") | .GroupId' | cut -d '"' -f2) --tags Key=Name,Value="$GROUPNAME"
+		echo "====================================================="
+	else
+		echo	 
+		echo "====================================================="
+		echo "Group $GROUPNAME Already Exists"
+		GROUPID=$(aws ec2 describe-security-groups --output=json | jq '.SecurityGroups | .[] | select(.GroupName=="Cloudfront") | .GroupId' | cut -d '"' -f2)
+		echo "====================================================="
+	fi
+	echo
+	echo
+	echo "====================================================="
+	echo "Adding rules to VPC Security Group "$GROUPNAME
+	echo "Records to be created: "$TOTALIPS
+	echo "====================================================="
+	echo
+	for ip in $IPLIST
+	do
+		RESULT=$(aws ec2 authorize-security-group-ingress --group-id "$GROUPID" --protocol $PROTO --port $PORT --cidr "$ip" 2>&1)
+		# Check for errors
+		if echo $RESULT | grep -q error; then
+			fail $RESULT
+		# else echo $RESULT
+		fi
+	done
+	echo "====================================================="
+	echo
+	tput setaf 2; echo "Completed!" && tput sgr0
+	echo
+}
+
+# Ensure Variables are set
+if [ "$VPCID" = "YOUR-VPC-ID-HERE" ]; then
+	fail "Failed to set variables!"
+fi
+
+# Verify AWS CLI Credentials are setup
+# http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html
+if ! grep -q aws_access_key_id ~/.aws/config; then
+	if ! grep -q aws_access_key_id ~/.aws/credentials; then
+		fail "Error: AWS config not found or CLI not installed. Please run \"aws configure\"."
+	fi
+fi
+
+# Check required commands
+check_command "curl"
+check_command "jq"
+
+addRules
