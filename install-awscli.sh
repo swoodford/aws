a030061a4c771e491a7ce99b812f58fd6bb2ba36
Shawn Woodford <sw@shawnwoodford.com>
2018-01-29 17:40:55 -0500

    updates

--- install-awscli.sh
+++ install-awscli.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # This script will install the AWS CLI
-# Requires Python, PIP
+# Requires Homebrew, Python, pip
 
 read -rp "Install and Configure AWS CLI? (y/n) " INSTALL
 if [[ $INSTALL =~ ^([yY][eE][sS]|[yY])$ ]]; then
 
-	# Test if PIP is installed
+	# Test if pip is installed
 	command -v pip >/dev/null 2>&1 || {
 		brew install python
 
-		echo "Installing PIP"
+		echo "Installing pip"
 		sudo easy_install pip
 	}
 
 	# Test if AWS CLI is installed
 	command -v aws >/dev/null 2>&1 || {
-		sudo pip install awscli
+		pip install awscli --upgrade --user
 
 		aws configure
 		complete -C '/usr/local/bin/aws_completer' aws
