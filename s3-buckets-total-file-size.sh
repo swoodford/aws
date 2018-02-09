671ad495f49ca9ea81b4d70542b457dc012044b0
Shawn Woodford <sw@shawnwoodford.com>
2018-02-09 10:58:35 -0500

    tweak

--- s3-buckets-total-file-size.sh
+++ s3-buckets-total-file-size.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# Script to count total size of all data stored in a single or in all S3 buckets (IAM account must have permission to access all buckets)
-# Requires aws s3api, jq
+# Script to count total size of all data stored in a single or in all S3 buckets
+# Requires aws s3api, jq, IAM account must have permission to access all buckets
 
 # Functions
 
