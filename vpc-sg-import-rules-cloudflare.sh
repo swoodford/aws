477e2e4c566aa688ed9bdb7bef870a88c50110ca
swoodford <>
2025-02-18 00:13:55 -0500

    bugfixes, support multiple ports, ipv6, better testing and validation

--- vpc-sg-import-rules-cloudflare.sh
+++ vpc-sg-import-rules-cloudflare.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
-# Create VPC Security Group with CloudFlare IP ranges
+# Create VPC Security Group with Cloudflare IP ranges
 # Requires curl, jq
 
-# Get current list of CloudFlare IP ranges
-IPLIST=$(curl -s https://www.cloudflare.com/ips-v4)
-
 # Set Variables
-GROUPNAME="CloudFlare"
-DESCR="CloudFlare IP Ranges"
+GROUPNAME="Cloudflare"
+DESCR="Cloudflare IP Ranges"
 VPCID="YOUR-VPC-ID-HERE"
 # VPCID=$(aws ec2 describe-vpcs --output=json | jq '.Vpcs | .[] | .VpcId' | cut -d '"' -f2)
 PROTO="tcp"
-PORT="443"
-TOTALIPS=$(echo "$IPLIST" | wc -l | tr -d ' ')
+# port parameter should be of the form <from[-to]> (e.g. 22 or 22-25)
+PORT="80,443"
+
+DEBUG=FALSE
 
 # Functions
 function pause(){
@@ -50,34 +49,92 @@ if [ $# -eq 0 ]; then
 	echo "Usage: ./$scriptname profile"
 	echo "Where profile is the AWS CLI profile name"
 	echo "Using default profile"
+	echo
 	profile=default
 else
 	profile=$1
 fi
 
+# Validate AWS CLI profile
+if ! aws sts get-caller-identity --profile $profile 2>&1 | grep -q "UserId"; then
+	fail "Invalid AWS CLI profile or credentials not setup. Please run \"aws configure\"."
+fi
+
+# Get current list of Cloudflare IP ranges
+IPV4LIST=$(curl -s https://www.cloudflare.com/ips-v4)
+if [ $? -ne 0 ]; then
+    fail "Failed to fetch Cloudflare IP ranges. Please check your internet connection and try again."
+fi
+if [ -z "$IPV4LIST" ]; then
+    fail "No IP ranges found. Please check the Cloudflare IP ranges URL."
+fi
+if $DEBUG; then
+	echo "IPV4LIST: $IPV4LIST"
+fi
+
+IPV6LIST=$(curl -s https://www.cloudflare.com/ips-v6)
+if [ $? -ne 0 ]; then
+	fail "Failed to fetch Cloudflare IPv6 ranges. Please check your internet connection and try again."
+fi
+if [ -z "$IPV6LIST" ]; then
+	fail "No IPv6 ranges found. Please check the Cloudflare IPv6 ranges URL."
+fi
+if $DEBUG; then
+	echo "IPV6LIST: $IPV6LIST"
+fi
+
+IPLIST=$(echo "$IPV4LIST")
+if $DEBUG; then
+	echo "IPLIST: $IPLIST"
+fi
+
+TOTALIPS=$(echo "$IPLIST" "$IPV6LIST" | wc -l | tr -d ' ')
+
+# Validate TOTALIPS variable
+if ! [[ "$TOTALIPS" =~ ^[0-9]+$ ]] || [ "$TOTALIPS" -le 0 ]; then
+    fail "Error fetching Cloudflare IP ranges: $IPLIST $TOTALIPS"
+else
+	echo "Number of Cloudflare IPs found: "$TOTALIPS
+fi
+
+# Validate PROTO variable
+if ! [[ "$PROTO" =~ ^[a-zA-Z]+$ || "$PROTO" == "-1" ]]; then
+    fail "Invalid PROTO value. It should contain only letters or be '-1'."
+fi
+
+# Validate PORT variable
+if [[ "$PORT" =~ , ]]; then
+    echo "PORT value: $PORT contains a comma, adding Inbound rules for more than one port value."
+fi
 
 function checkGroups (){
 	# Check for existing security group or create new one
-	if ! aws ec2 describe-security-groups --output=json --profile $profile 2>&1 | jq '.SecurityGroups | .[] | .GroupName' | grep -q CloudFlare; then
+	checkGroups=$(aws ec2 describe-security-groups --output=json --profile $profile 2>&1 | jq '.SecurityGroups | .[] | .GroupName')
+	if ! echo "$checkGroups" | grep -q "$GROUPNAME"; then
 		echo
 		echo "====================================================="
-		echo "Creating Security Group "$GROUPNAME
+		echo "Creating Security Group: "$GROUPNAME
 		GROUPID=$(aws ec2 create-security-group --group-name "$GROUPNAME" --description "$DESCR" --vpc-id $VPCID --profile $profile 2>&1 | jq '.GroupId' | cut -d '"' -f2)
-		echo $GROUPID
-		aws ec2 create-tags --resources $(aws ec2 describe-security-groups --output=json --profile $profile 2>&1 | jq '.SecurityGroups | .[] | select(.GroupName=="CloudFlare") | .GroupId' | cut -d '"' -f2) --tags Key=Name,Value="$GROUPNAME" --profile $profile 2>&1
+		if $DEBUG; then
+			echo "GROUPID: $GROUPID"
+		fi
+		aws ec2 create-tags --resources $(aws ec2 describe-security-groups --output=json --profile $profile 2>&1 | jq '.SecurityGroups | .[] | select(.GroupName=="'"$GROUPNAME"'") | .GroupId' | cut -d '"' -f2) --tags Key=Name,Value="$GROUPNAME" --profile $profile 2>&1
 		echo "====================================================="
 
 		addRules
+		addRulesV6
 	else
 		echo
 		echo "====================================================="
 		echo "Group $GROUPNAME Already Exists"
-		GROUPID=$(aws ec2 describe-security-groups --output=json --profile $profile 2>&1 | jq '.SecurityGroups | .[] | select(.GroupName=="CloudFlare") | .GroupId' | cut -d '"' -f2)
+		GROUPID=$(aws ec2 describe-security-groups --output=json --profile $profile 2>&1 | jq '.SecurityGroups | .[] | select(.GroupName=="'"$GROUPNAME"'") | .GroupId' | cut -d '"' -f2)
+		echo "Security Group ID: $GROUPID"
 		read -r -p "Do you want to delete the group and recreate it? (y/n) " DELETEGROUP
 		if [[ $DELETEGROUP =~ ^([yY][eE][sS]|[yY])$ ]]; then
 			echo
 			echo "====================================================="
-			echo "Deleting Group Name $GROUPNAME, Security Group ID $GROUPID"
+			echo "Deleting Group Name: $GROUPNAME"
+			echo "Security Group ID: $GROUPID"
 			echo "====================================================="
 			DELETEGROUP=$(aws ec2 delete-security-group --group-id "$GROUPID" --profile $profile 2>&1)
 			if echo $DELETEGROUP | grep -q error; then
@@ -85,18 +142,20 @@ function checkGroups (){
 			else
 				echo
 				echo "====================================================="
-				echo "Creating Security Group "$GROUPNAME
+				echo "Creating Security Group: "$GROUPNAME
 				GROUPID=$(aws ec2 create-security-group --group-name "$GROUPNAME" --description "$DESCR" --vpc-id $VPCID --profile $profile 2>&1 | jq '.GroupId' | cut -d '"' -f2)
 				echo $GROUPID
-				aws ec2 create-tags --resources $(aws ec2 describe-security-groups --output=json --profile $profile 2>&1 | jq '.SecurityGroups | .[] | select(.GroupName=="CloudFlare") | .GroupId' | cut -d '"' -f2) --tags Key=Name,Value="$GROUPNAME" --profile $profile 2>&1
+				aws ec2 create-tags --resources $(aws ec2 describe-security-groups --output=json --profile $profile 2>&1 | jq '.SecurityGroups | .[] | select(.GroupName=="'"$GROUPNAME"'") | .GroupId' | cut -d '"' -f2) --tags Key=Name,Value="$GROUPNAME" --profile $profile 2>&1
 				echo "====================================================="
 
 				addRules
+				addRulesV6
 			fi
 		else
 			read -r -p "Do you want to add additional IPs to the existing group? (y/n) " ADDGROUP
 			if [[ $ADDGROUP =~ ^([yY][eE][sS]|[yY])$ ]]; then
 				addRules
+				addRulesV6
 			else
 				echo "Exiting"
 				exit 1
@@ -108,29 +167,140 @@ function checkGroups (){
 function addRules (){
 	echo
 	echo "====================================================="
-	echo "Adding rules to VPC Security Group "$GROUPNAME
-	echo "Records to be created: "$TOTALIPS
+	echo "Adding rules to VPC Security Group: "$GROUPNAME
 	echo "====================================================="
 	echo
-	for ip in $IPLIST
-	do
-		RESULT=$(aws ec2 authorize-security-group-ingress --group-id "$GROUPID" --protocol $PROTO --port $PORT --cidr "$ip" --profile $profile 2>&1)
-		# Check for errors
-		if echo $RESULT | grep -q error; then
-			fail $RESULT
-		# else echo $RESULT
-		fi
-	done
-	echo "====================================================="
-	echo
-	tput setaf 2; echo "Completed!" && tput sgr0
-	echo
+	if $DEBUG; then
+		echo "GROUPNAME: $GROUPNAME"
+		echo "DESCR: $DESCR"
+		echo "VPCID: $VPCID"
+		echo "PROTO: $PROTO"
+		echo "PORT: $PORT"
+	fi
+	if [[ "$PORT" =~ , ]]; then
+		# Split PORT into an array
+		IFS=',' read -r -a PORTS <<< "$PORT"
+		for PORT in "${PORTS[@]}"
+		do
+			if ! [[ "$PORT" =~ ^[0-9]+(-[0-9]+)?$ ]]; then
+				fail "Invalid PORT value: $PORT It should be a single numeric value or a range in the form <from[-to]>."
+			fi
+			if $DEBUG; then
+				echo "PORT: $PORT"
+			fi
+			for ip in $IPLIST
+			do
+				# if $DEBUG; then
+				# 	echo "IP: $ip"
+				# fi
+				echo "IP: $ip Port: $PORT Protocol: $PROTO"
+				RESULT=$(aws ec2 authorize-security-group-ingress --group-id "$GROUPID" --protocol $PROTO --port $PORT --cidr "$ip" --profile $profile 2>&1)
+				# Check for errors
+				if echo "$RESULT" | grep -q error; then
+					fail "$RESULT"
+				else
+					if $DEBUG; then
+						echo "$RESULT"
+					fi
+				fi
+			done
+		done
+	else
+		for ip in $IPLIST
+		do
+			# if $DEBUG; then
+			# 	echo "IP: $ip"
+			# fi
+			if ! [[ "$PORT" =~ ^[0-9]+(-[0-9]+)?$ ]]; then
+				fail "Invalid PORT value: $PORT It should be a single numeric value or a range in the form <from[-to]>."
+			fi
+			echo "IP: $ip Port: $PORT Protocol: $PROTO"
+			RESULT=$(aws ec2 authorize-security-group-ingress --group-id "$GROUPID" --protocol $PROTO --port $PORT --cidr "$ip" --profile $profile 2>&1)
+			# Check for errors
+			if echo "$RESULT" | grep -q error; then
+				fail "$RESULT"
+			else
+				if $DEBUG; then
+					echo "$RESULT"
+				fi
+			fi
+		done
+	fi
 }
 
-
+function addRulesV6 (){
+	IPLIST=$(echo "$IPV6LIST")
+	echo
+	echo "====================================================="
+	echo "Adding IPv6 rules to VPC Security Group: "$GROUPNAME
+	echo "====================================================="
+	echo
+	if $DEBUG; then
+		echo "GROUPNAME: $GROUPNAME"
+		echo "DESCR: $DESCR"
+		echo "VPCID: $VPCID"
+		echo "PROTO: $PROTO"
+		echo "PORT: $PORT"
+	fi
+	if [[ "$PORT" =~ , ]]; then
+		# Split PORT into an array
+		IFS=',' read -r -a PORTS <<< "$PORT"
+		for PORT in "${PORTS[@]}"
+		do
+			if ! [[ "$PORT" =~ ^[0-9]+(-[0-9]+)?$ ]]; then
+				fail "Invalid PORT value: $PORT It should be a single numeric value or a range in the form <from[-to]>."
+			fi
+			if $DEBUG; then
+				echo "PORT: $PORT"
+			fi
+			for ip in $IPLIST
+			do
+				echo "IP: $ip Port: $PORT Protocol: $PROTO"
+				# RESULT=$(aws ec2 authorize-security-group-ingress --group-id "$GROUPID" --protocol $PROTO --port $PORT --cidr "$ip" --profile $profile 2>&1)
+				RESULT=$(aws ec2 authorize-security-group-ingress --group-id "$GROUPID" --ip-permissions IpProtocol=$PROTO,FromPort=$PORT,ToPort=$PORT,Ipv6Ranges=[{CidrIpv6="$ip"}] --profile $profile 2>&1)
+				# Check for errors
+				if echo "$RESULT" | grep -q error; then
+					fail "$RESULT"
+				else
+					if $DEBUG; then
+						echo "$RESULT"
+					fi
+				fi
+			done
+		done
+	else
+		for ip in $IPLIST
+		do
+			if ! [[ "$PORT" =~ ^[0-9]+(-[0-9]+)?$ ]]; then
+				fail "Invalid PORT value: $PORT It should be a single numeric value or a range in the form <from[-to]>."
+			fi
+			echo "IP: $ip Port: $PORT Protocol: $PROTO"
+			# RESULT=$(aws ec2 authorize-security-group-ingress --group-id "$GROUPID" --protocol $PROTO --port $PORT --cidr "$ip" --profile $profile 2>&1)
+			RESULT=$(aws ec2 authorize-security-group-ingress --group-id "$GROUPID" --ip-permissions IpProtocol=$PROTO,FromPort=$PORT,ToPort=$PORT,Ipv6Ranges=[{CidrIpv6="$ip"}] --profile $profile 2>&1)
+			# Check for errors
+			if echo "$RESULT" | grep -q error; then
+				fail "$RESULT"
+			else
+				if $DEBUG; then
+					echo "$RESULT"
+				fi
+			fi
+		done
+	fi
+}
 
 # Check required commands
 check_command "curl"
 check_command "jq"
 
 checkGroups
+
+echo "====================================================="
+echo
+checkGroups=$(aws ec2 describe-security-groups --output=json --profile $profile 2>&1 | jq '.SecurityGroups | .[] | .GroupName')
+if ! echo "$checkGroups" | grep -q "$GROUPNAME"; then
+	fail "Failed to create security group!"
+else
+	echo "Security Group Created: $GROUPNAME: $GROUPID"
+	tput setaf 2; echo "Completed!" && tput sgr0
+fi
